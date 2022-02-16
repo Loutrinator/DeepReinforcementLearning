93df2e5b3b5b8c780a1cf065409cf15a929b4b68
sikny <pamartqu@gmail.com>
2022-02-16 15:01:54 +0100

    layer arrival & gridsize + layermask

--- Assets/Scripts/GridPlayer.cs
+++ Assets/Scripts/GridPlayer.cs
@@ -2,11 +2,12 @@
 
 public class GridPlayer : MonoBehaviour {
     public float gridSize = 1;
+    [SerializeField] private LayerMask walkableMask;
 
     public virtual void Move(Vector2 direction) {
-        Vector3 destination = transform.position + direction.x * Vector3.right + direction.y * Vector3.up;
+        Vector3 destination = transform.position + (direction.x * Vector3.right + direction.y * Vector3.up) * gridSize;
         if (Physics.Raycast(destination + Vector3.back * 5, Vector3.forward, out var hit, 10)) {
-            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Ground")) {
+            if (walkableMask == (walkableMask | (1 << hit.collider.gameObject.layer))){
                 transform.position = destination;
             }
         }
