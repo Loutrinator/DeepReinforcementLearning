48fe42f13a333cbce17ec424ab6c146b8067d165
sikny <pamartqu@gmail.com>
2022-02-17 10:35:47 +0100

    wip value iteration

--- Assets/Scripts/ReinforcementLearning/DynamicProgramming.cs
+++ Assets/Scripts/ReinforcementLearning/DynamicProgramming.cs
@@ -1,4 +1,7 @@
-﻿namespace ReinforcementLearning {
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ReinforcementLearning {
     // ReSharper disable once InconsistentNaming
     internal class MDPState {
         public Movement action;
@@ -8,17 +11,39 @@
             action = actionP;
         }
     }
-    public class DynamicProgramming {
+    public static class DynamicProgramming {
         public static void PolicyIteration(GridPlayer player, GameGrid grid) {
             // init
-            var possibleStates = player.GetAllPossibleStates(grid);
-            foreach (var possibleState in possibleStates) {
-                
+            var possibleGridStates = player.GetAllPossibleStates(grid);
+            foreach (var possibleState in possibleGridStates) {
+                GridState state = new GridState(possibleState) {
+                    value = 0
+                };
             }
         }
-        public static void ValueIteration() {
+        public static void ValueIteration(GridPlayer player, GameGrid grid) {
             // init
-            
+            var possibleGridStates = player.GetAllPossibleStates(grid);
+            var possibleStates = new List<GridState>();
+            foreach (var possibleState in possibleGridStates) {
+                GridState state = new GridState(possibleState) {
+                    value = 0
+                };
+                possibleStates.Add(state);
+            }
+
+            float delta;
+            float theta = 0.7f;
+            do {
+                delta = 0;
+                foreach (var state in possibleStates) {
+                    float temp = state.value;
+                    
+                    // todo
+
+                    delta = Mathf.Max(delta, Mathf.Abs(temp - state.value));
+                }
+            } while (delta < theta);
         }
     }
 }
--- Assets/Scripts/ReinforcementLearning/GridState.cs
+++ Assets/Scripts/ReinforcementLearning/GridState.cs
@@ -1,6 +1,7 @@
 ﻿namespace ReinforcementLearning {
     public class GridState {
         public int[][] grid;
+        public float value;
 
         public GridState(int[][] gridP) {
             grid = gridP;
