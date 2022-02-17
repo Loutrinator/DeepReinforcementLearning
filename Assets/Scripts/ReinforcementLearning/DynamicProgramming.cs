b509a23bbfba2c2c3043b29a65c9e7e5e1a89843
sikny <pamartqu@gmail.com>
2022-02-17 15:04:54 +0100

    wip value iteration

--- Assets/Scripts/ReinforcementLearning/DynamicProgramming.cs
+++ Assets/Scripts/ReinforcementLearning/DynamicProgramming.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace ReinforcementLearning {
@@ -16,30 +17,51 @@ namespace ReinforcementLearning {
             // init
             var possibleGridStates = player.GetAllPossibleStates(grid);
             var possibleStates = new List<GridState>();
-            string log = "";
             foreach (var possibleState in possibleGridStates) {
                 GridState state = new GridState(possibleState) {
                     value = 0
                 };
-                log += state + "\n";
                 possibleStates.Add(state);
             }
-            Debug.Log(log);
 
-            return;
+            float gamma = 0.9f; // facteur de dévaluation
             float delta;
             float theta = 0.7f;
+            int iterations = 0;
             do {
+                ++iterations;
                 delta = 0;
-                foreach (var state in possibleStates) {
+                for (var index = 0; index < possibleStates.Count; index++) {
+                    var state = possibleStates[index];
                     float temp = state.value;
-                    
-                    // todo
-                    //state.value = 
+
+                    float max = 0;
+                    foreach (Movement actionType in Enum.GetValues(typeof(Movement))) {
+                        float reward = state.GetReward(actionType, possibleStates, out var nextStateTmp);
+                        float currentVal = reward + gamma * nextStateTmp.value;
+                        if (max < currentVal) {
+                            state.bestAction = actionType;
+                            max = currentVal;
+                        }
+                    }
+
+                    state.value = max;
 
                     delta = Mathf.Max(delta, Mathf.Abs(temp - state.value));
                 }
-            } while (delta < theta);
+            } while (delta > theta);
+            Debug.Log(iterations + " iterations");
+            
+            // build end policy
+            List<Movement> policy = new List<Movement>();
+            var currentState = grid.gridState;
+            policy.Add(grid.gridState.bestAction);
+            var nextState = currentState.GetNextState(currentState.bestAction, possibleStates, out _, out _);
+            policy.Add(nextState.bestAction);
+
+            for (int i = 0; i < policy.Count; ++i) {
+                Debug.Log(i + " => " + policy[i]);
+            }
         }
     }
 }
--- Assets/Scripts/ReinforcementLearning/GridState.cs
+++ Assets/Scripts/ReinforcementLearning/GridState.cs
@@ -1,7 +1,11 @@
-﻿namespace ReinforcementLearning {
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ReinforcementLearning {
     public class GridState {
         public int[][] grid;
         public float value;
+        public Movement bestAction = Movement.Down;
 
         public GridState(int[][] gridP) {
             grid = gridP;
@@ -19,6 +23,82 @@
             }
             
             return log;
+        }
+
+        public int GetReward(Movement action, List<GridState> possibleStates, out GridState nextState) {
+            var layerArrival = LayerMask.NameToLayer("Arrival");
+            nextState = GetNextState(action, possibleStates, out var playerI, out var playerJ);
+            if (nextState.grid[playerI][playerJ] == layerArrival) {
+                return 1;
+            }
+
+            return 0;
+        }
+
+        public GridState GetNextState(Movement action, List<GridState> possibleStates, out int playerNewI, out int playerNewJ) {
+            var layerPlayer = LayerMask.NameToLayer("Player");
+            var layerGround = LayerMask.NameToLayer("Ground");
+            var layerArrival = LayerMask.NameToLayer("Arrival");
+            var nextState = new GridState(grid.CloneGrid());
+            playerNewI = -1;
+            playerNewJ = -1;
+            Vector2Int gridSize = new Vector2Int(grid[0].Length, grid.Length);
+            for (int i = 0; i < gridSize.y; ++i) {
+                for (int j = 0; j < gridSize.x; ++j) {
+                    if (grid[i][j] == layerPlayer) {
+                        int testCell = 0;
+                        int newI = i, newJ = j;
+                        switch (action) {
+                            case Movement.Up:
+                                newI = i - 1 > 0 ? i - 1 : i;
+                                break;
+                            case Movement.Right:
+                                newJ = j + 1 < gridSize.x ? j + 1 : j;
+                                break;
+                            case Movement.Down:
+                                newI = i + 1 < gridSize.y ? i + 1 : i;
+                                break;
+                            case Movement.Left:
+                                newJ = j - 1 > 0 ? j - 1 : j;
+                                break;
+                        }
+
+                        playerNewI = newI;
+                        playerNewJ = newJ;
+                        if (testCell == layerGround || testCell == layerArrival) {
+                            nextState.grid[i][j] = layerGround;
+                            nextState.grid[newI][newJ] = layerPlayer;
+
+                            return possibleStates.Find(state => state.Equals(nextState));
+                        }
+                    }
+                }
+            }
+            
+            return possibleStates.Find(state => state.Equals(nextState));
+        }
+
+        public override bool Equals(object other) {
+            return Equals((GridState)other);
+        }
+
+        protected bool Equals(GridState other) {
+            Vector2Int gridSize = new Vector2Int(grid[0].Length, grid.Length);
+            for (int i = 0; i < gridSize.y; ++i) {
+                for (int j = 0; j < gridSize.x; ++j) {
+                    if (grid[i][j] != other.grid[i][j]) return false;
+                }
+            }
+            return true;
+        }
+
+        public override int GetHashCode() {
+            unchecked {
+                var hashCode = (grid != null ? grid.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ value.GetHashCode();
+                hashCode = (hashCode * 397) ^ (int)bestAction;
+                return hashCode;
+            }
         }
     }
 }
