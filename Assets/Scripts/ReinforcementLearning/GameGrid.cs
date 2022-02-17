f8e86e839a740823edb38c09d98e1fa1c5c400d8
sikny <pamartqu@gmail.com>
2022-02-17 17:15:31 +0100

    fix inddex y grid

--- Assets/Scripts/ReinforcementLearning/GameGrid.cs
+++ Assets/Scripts/ReinforcementLearning/GameGrid.cs
@@ -8,13 +8,13 @@ namespace ReinforcementLearning {
 
         public void Init(Transform groundPlane) {
             var groundScale = groundPlane.lossyScale;
-            var groundSize = new Vector2(groundScale.x * 10, groundScale.y * 10);
+            var groundSize = new Vector2Int(Mathf.RoundToInt(groundScale.x * 10), Mathf.RoundToInt(groundScale.z * 10));
             var groundPos = groundPlane.position;
-            var firstPos = new Vector3(groundPos.x - groundSize.x / 2, groundPos.y - groundSize.y / 2, 0);
+            var firstPos = new Vector3(groundPos.x - groundSize.x / 2f, groundPos.y - groundSize.y / 2f, 0);
 
-            var grid = new int[(int)groundSize.y][];
+            var grid = new int[groundSize.y][];
             for (int i = 0; i < grid.Length; ++i) {
-                grid[i] = new int[(int)groundSize.x];
+                grid[i] = new int[groundSize.x];
 
                 for (int j = 0; j < grid[i].Length; ++j) {
                     Vector3 castPos = firstPos + j * Vector3.right + i * Vector3.up + new Vector3(0.5f, 0.5f, 0);
